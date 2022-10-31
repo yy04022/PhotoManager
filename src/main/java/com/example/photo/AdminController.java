8312982d8540fc3401cd404944a81f02e620b053
JinyuWang <ellie5201314@gmail.com>
2022-10-31 15:17:09 -0400

    10.31update

---
+++ src/main/java/com/example/photo/AdminController.java
@@ -0,0 +1,14 @@
+package com.example.photo;
+
+import javafx.event.ActionEvent;
+
+public class Adminpage {
+    public void deleteUserButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void listUserButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void addUserButtonClick(ActionEvent actionEvent) {
+    }
+}
